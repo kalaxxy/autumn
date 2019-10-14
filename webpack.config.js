34d9fca8e5abf73f45ac65a07f87f3117a5fac86
Katya Ivanenko <kalaxxy@yandex.ru>
2019-10-14 14:35:55 +0300

    изменен путь dist

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = '/autumn/';
+  const publicPath = '/autumn/dist/';
 
   const pcss = {
     test: /\.(p|post|)css$/,
